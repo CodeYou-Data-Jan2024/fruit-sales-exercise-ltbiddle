7b0dad43ab113357f6b194f75ebd1c71c37dc4be
Lance Biddle <lancebiddle@MacBookPro.attlocal.net>
2024-09-22 13:05:14 -0400

    Homework upload

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,10 @@
 # add your code here
+import pandas as pd
+
+data = {'Apples': [35, 21], 'Bannanas': [41, 34]}
+
+df = pd.DataFrame(data, index=['2017 Sales', '2018 Sales'])
+
+df.to_csv('/Users/lancebiddle/CodeYou/fruit-sales-exercise-ltbiddle/fruit.csv')
+
+print(df)
